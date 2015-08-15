20e1041a1ff3be4c5fa90f0c84c0d1ed9c809d89
Denis Doan <dedoin08@yahoo.de>
2015-08-15 17:42:37 +0100

    5

--- index.js
+++ index.js
@@ -5,4 +5,5 @@ var grid = document.getElementsByClassName('grid');
 var i;
 for (i = 0; i < grid.length; i++) {
     grid[i].addEventListener("click", function() {  
-document.getElementsByClassName("grid-element").innerHTML = "X"; })};
+document.getElementById("grid-element").innerHTML = "X";
+ })};
