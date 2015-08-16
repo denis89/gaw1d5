3b262aa6c63189e20f392b13fef0b5073fcbf548
Denis Doan <dedoin08@yahoo.de>
2015-08-16 15:12:34 +0100

    6

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
-
 // JS on each Gameboard field.
 var grid = document.getElementsByClassName('grid');
 
-var i;
-for (i = 0; i < grid.length; i++) {
-    grid[i].addEventListener("click", function() {  
-document.getElementById("grid-element").innerHTML = "X";
- })};
+for (var i = 0; i < grid.length; i++) {
+    //grid[i].value = "X";
+    addEventListener("click", function() {  
+//
+document.getElementById("grid-elementa").innerHTML = "X";
+
+document.getElementById("grid-elementb").innerHTML = "X";
+
+document.getElementById("grid-elementc").innerHTML = "X";
+
+document.getElementById("grid-elementd").innerHTML = "X";
+
+document.getElementById("grid-elemente").innerHTML = "X";
+
+document.getElementById("grid-elementf").innerHTML = "X";
+
+
+ }
+
+ )};
